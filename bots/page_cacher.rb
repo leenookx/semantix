c06f0c54340d41bd6b9d2b3eb64098cc00b52943
lnookx <lnookx@googlemail.com>
2009-06-23 13:29:00 +0100

    allow the page cacher to get the data from the web and update the database accordingly.

--- bots/page_cacher.rb
+++ bots/page_cacher.rb
@@ -2,8 +2,25 @@
 
 require 'rubygems'
 require 'simplehttp'
+require 'active_record'
 
-data = SimpleHttp.get "http://slashdot.org"
+link_id = 1
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+ActiveRecord::Base.colorize_logging = true
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :dbfile => "db/development.sqlite3"
+)
+
+require 'app/models/link.rb'
+
+record = Link.find( link_id )
+url = record.url
+puts url
+
+data = SimpleHttp.get url
 
 filename = "local_cache/"+`uuidgen`.strip+".tmp"
 afile = File.new(filename, "w+")
@@ -11,4 +28,7 @@ afile = File.new(filename, "w+")
 afile.puts data
 
 afile.close
+
+Link.update(link_id, :cache_file => filename, :processed => 1 )
+
 
