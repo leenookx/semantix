edd2d2e85f384db9e188f03230cf69369e284f0e
lnookx <lnookx@googlemail.com>
2009-08-16 16:57:15 +0100

    Make sure that the Feeds page can be accessed through the application.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090816153617) do
+ActiveRecord::Schema.define(:version => 20090816155239) do
+
+  create_table "feeds", :force => true do |t|
+    t.string   "url"
+    t.integer  "interval"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "keywords", :force => true do |t|
     t.string   "keyword"
