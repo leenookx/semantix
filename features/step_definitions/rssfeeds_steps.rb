6472c0a83b4e21ff557bc6142fd58ccb20f260dc
lnookx <lnookx@googlemail.com>
2009-08-17 04:25:23 +0100

    Initial cucumber tests for the rss feed code.

---
+++ features/step_definitions/rssfeeds_steps.rb
@@ -0,0 +1,12 @@
+Given /^that the user wants to add a feed$/ do
+  pending
+end
+
+When /^the user enters the feed details$/ do
+  pending
+end
+
+Then /^the system should save them$/ do
+  pending
+end
+
