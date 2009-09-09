0451c1ee1dbc38fe982c02209405b6a9cf13292c
lnookx <lnookx@googlemail.com>
2009-09-09 17:38:32 +0100

    Make sure all of the dependant gems are setup in the environment file.

--- config/environment.rb
+++ config/environment.rb
@@ -15,10 +15,17 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "ferret"
+  config.gem "acts_as_ferret"
+  config.gem "chronic"
+  config.gem "packet"
+  config.gem "feed-normalizer"
+  config.gem "workling"
+  config.gem "memcached"
+  config.gem "memcache-client"
+  config.gem "daemons"
+  config.gem "starling"
+  config.gem "leenookx-feed-utils", :source => "http://github.com"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
