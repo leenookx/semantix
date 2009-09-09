a2c724cf505d4249944f9fd21c11308553580a7e
lnookx <lnookx@googlemail.com>
2009-09-09 18:16:47 +0100

    Add the new implementation for the topix REST service.

--- app/controllers/topix_controller.rb
+++ app/controllers/topix_controller.rb
@@ -1,4 +1,19 @@
+require 'feed-utils'
+
 class TopixController < ApplicationController
+  def index 
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+  
   def show
+    topix = TopixGrabber.new
+    @topix_data = topix.query(params[:id])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.rss  { render :rss => @topix_data }
+    end
   end
 end
