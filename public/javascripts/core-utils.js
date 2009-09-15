5bd9cf294ef8ae8c327f3cf19e2663db7de43328
lnookx <lnookx@googlemail.com>
2009-09-15 22:16:44 +0100

    More tag cloud goodness.

--- public/javascripts/core-utils.js
+++ public/javascripts/core-utils.js
@@ -13,3 +13,31 @@ function sanitize(string){
   return encodeURI(string);
 }
 
+function Countdown(options) {
+  var timer,
+  instance = this,
+  seconds = options.seconds || 10,
+  updateStatus = options.onUpdateStatus || function () {},
+  counterEnd = options.onCounterEnd || function () {};
+
+  function decrementCounter() {
+    updateStatus(seconds);
+    if (seconds === 0) {
+      counterEnd();
+      instance.stop();
+    }
+    seconds--;
+  }
+
+  this.start = function () {
+    clearInterval(timer);
+    timer = 0;
+    seconds = options.seconds;
+    timer = setInterval(decrementCounter, 1000);
+  };
+
+  this.stop = function () {
+    clearInterval(timer);
+  };
+}
+
--- public/javascripts/tagcloud-utils.js
+++ public/javascripts/tagcloud-utils.js
@@ -1,10 +1,56 @@
-function addToTagCloud(tag, url, src) {
+var myTagCloudTimer = new Countdown({
+                        seconds: 5,
+                        onCounterEnd: function() {
+                          updateTagCloud();
+                        }
+                      });
+
+function createNewTagCloud(tag, url, src) {
   var req = new Ajax.Request("/tagcloud/", {
           method: 'POST',
           onFailure: function(request) {
             alert('AJAX doesnt work.');
           },
+          parameters: {id: 1}
+      });
+}
+
+function addToTagCloud(tag, url, src) {
+  myTagCloudTimer.stop();
+
+  var req = new Ajax.Request("/tagcloud/", {
+          method: 'PUT',
+          onFailure: function(request) {
+            alert('AJAX doesnt work.');
+          },
           parameters: {tag: tag, url: url, src: src}
       });
+
+  myTagCloudTimer.start();
+}
+
+function updateTagCloud() {
+  var req = new Ajax.Request("/tagcloud/1.json", {
+          method: 'GET',
+          onFailure: function(request) {
+            alert('AJAX doesnt work.');
+          },
+          onComplete: function(request) {
+            receiveTagCloudResponse( request.responseXML );
+          }
+      });
+}
+
+function receiveTagCloudResponse( response ) {
+  if (response != "") {
+    var doc = JSON.parse(response);
+    var tags = doc['tags'];
+    var point = document.getElementById('tagcloud');
+    for (var i = 0; i < tags.length; i++) {
+    }
+  }
+  else {
+    showStatus("Something went wrong with the retrieval of the tag cloud information.");
+  }
 }
 
